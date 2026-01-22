b7ebd2ad50f9dc1d7339faf622991a477b97cda6
titangene <titangene.tw@gmail.com>
2026-01-22 22:18:23 +0800

    refactor: can check first elements equality

--- src/ProbabilitySystem.ts
+++ src/ProbabilitySystem.ts
@@ -1,6 +1,21 @@
 export class ProbabilitySystem {
+  reels: Array<Array<string>> = [
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', '10', 'J']
+  ];
+
   spin(betLine: string): number {
-    if (betLine === 'L1') {
+    const firstElementsSet = new Set<string>();
+
+    for (let i = 0; i < this.reels.length; i++) {
+      const reel = this.reels[i];
+      firstElementsSet.add(reel[0]);
+    }
+
+    if (firstElementsSet.size === 1 && betLine === 'L1') {
       return 20;
     }
     return 0;
