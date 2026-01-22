b85c23427a188acd9bb25126594425191b6ad85b
titangene <titangene.tw@gmail.com>
2026-01-22 23:15:55 +0800

    refactor: extract isRowHit()

--- src/Reels.ts
+++ src/Reels.ts
@@ -2,22 +2,21 @@ export class Reels {
   constructor(private reels: Array<Array<string>>) {}
 
   isRow1Hit() {
-    const uniqueElements = new Set<string>();
-
-    for (let i = 0; i < this.reels.length; i++) {
-      const reel = this.reels[i];
-      uniqueElements.add(reel[0]);
-    }
-
-    return uniqueElements.size === 1;
+    const row = 0;
+    return this.isRowHit(row);
   }
 
   isRow2Hit() {
+    const row = 1;
+    return this.isRowHit(row);
+  }
+
+  isRowHit(row: number): boolean {
     const uniqueElements = new Set<string>();
 
     for (let i = 0; i < this.reels.length; i++) {
       const reel = this.reels[i];
-      uniqueElements.add(reel[1]);
+      uniqueElements.add(reel[row]);
     }
 
     return uniqueElements.size === 1;
