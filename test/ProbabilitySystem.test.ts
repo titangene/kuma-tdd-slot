9e4b5189470b1263c77c4273643eea38f8d38361
titangene <titangene.tw@gmail.com>
2026-01-22 22:11:48 +0800

    red: L1 hit -> 20

--- test/ProbabilitySystem.test.ts
+++ test/ProbabilitySystem.test.ts
@@ -4,6 +4,11 @@ import { ProbabilitySystem } from '@/ProbabilitySystem.ts';
 describe('probability system', () => {
   test('lose', () => {
     const sut = new ProbabilitySystem();
-    expect(sut.spin()).toBe(0);
+    expect(sut.spin('L2')).toBe(0);
+  });
+
+  test('L1 hit -> 20', () => {
+    const sut = new ProbabilitySystem();
+    expect(sut.spin('L1')).toBe(20);
   });
 });
