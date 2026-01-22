963ad0c7e9e2c922a5f19a4b3c889fe693620a00
titangene <titangene.tw@gmail.com>
2026-01-22 23:05:23 +0800

    green: Row2 hit, bet L2 -> 0

--- src/ProbabilitySystem.ts
+++ src/ProbabilitySystem.ts
@@ -7,6 +7,20 @@ export class ProbabilitySystem {
     if (this.reels.isRow1Hit() && betLine === 'L1') {
       return 20;
     }
+
+    const secondElementsSet = new Set<string>();
+
+    for (let i = 0; i < this.reels.reels.length; i++) {
+      const reel = this.reels.reels[i];
+      secondElementsSet.add(reel[1]);
+    }
+
+    const isRow2Hit = secondElementsSet.size === 1;
+
+    if (isRow2Hit && betLine === 'L2') {
+      return 20;
+    }
+
     return 0;
   }
 }
