4948627fc98c4b0a807a2d3dd66c755b3e3eefa2
titangene <titangene.tw@gmail.com>
2026-01-23 10:49:24 +0800

    refactor: update isHit() signature to improve parameter clarity

--- src/PayTable.ts
+++ src/PayTable.ts
@@ -3,22 +3,22 @@ import { Bet } from './Bet.ts';
 
 export class PayTable {
   getOdd(betLines: string[], reels: Reels): number {
-    if (reels.isRowHit(0) && this.isHit(betLines, 'L1', new Bet(betLines))) {
+    if (reels.isRowHit(0) && this.isHit(new Bet(betLines), 'L1')) {
       return 20;
     }
 
-    if (reels.isRowHit(1) && this.isHit(betLines, 'L2', new Bet(betLines))) {
+    if (reels.isRowHit(1) && this.isHit(new Bet(betLines), 'L2')) {
       return 20;
     }
 
-    if (reels.isRowHit(2) && this.isHit(betLines, 'L3', new Bet(betLines))) {
+    if (reels.isRowHit(2) && this.isHit(new Bet(betLines), 'L3')) {
       return 20;
     }
 
     return 0;
   }
 
-  private isHit(betLines: string[], line: string, bet: Bet): boolean {
+  private isHit(bet: Bet, line: string): boolean {
     return bet.includes(line);
   }
 }
