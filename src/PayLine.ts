139451eb24044facf5c43cdacd104212768dbac2
titangene <titangene.tw@gmail.com>
2026-01-23 14:39:13 +0800

    refactor: remove unused code

--- src/PayLine.ts
+++ src/PayLine.ts
@@ -25,18 +25,6 @@ export class PayLine {
       odds.find(odd => odd.hitLength === screen.getHitLength(this.rows))?.odd ??
       0
     );
-
-    if (bet.includes(this.name)) {
-      if (screen.getHitLength(this.rows) === 5) {
-        return 20;
-      } else if (screen.getHitLength(this.rows) === 4) {
-        return 15;
-      } else {
-        return 0;
-      }
-    } else {
-      return 0;
-    }
   }
 
   static from(name: string, rows: number[]): PayLine {
