c379979fd5a8bebe7ba04d7694cc3925d1ea12f0
titangene <titangene.tw@gmail.com>
2026-01-23 14:27:48 +0800

    refactor: introduce Odd class

--- src/PayLine.ts
+++ src/PayLine.ts
@@ -1,6 +1,13 @@
 import { Screen } from './Screen.ts';
 import { Bet } from './Bet.ts';
 
+class Odd {
+  constructor(
+    public hitLength: number,
+    public odd: number
+  ) {}
+}
+
 export class PayLine {
   private constructor(
     private name: string,
@@ -8,6 +15,17 @@ export class PayLine {
   ) {}
 
   getOdd(screen: Screen, bet: Bet): number {
+    if (!bet.includes(this.name)) {
+      return 0;
+    }
+
+    const odds: Odd[] = [new Odd(5, 20), new Odd(4, 15)];
+
+    return (
+      odds.find(odd => odd.hitLength === screen.getHitLength(this.rows))?.odd ??
+      0
+    );
+
     if (bet.includes(this.name)) {
       if (screen.getHitLength(this.rows) === 5) {
         return 20;
