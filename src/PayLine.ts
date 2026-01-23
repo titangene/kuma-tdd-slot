baddd2ed98f28e9067beb264d02060dd1098e73f
titangene <titangene.tw@gmail.com>
2026-01-23 14:58:19 +0800

    feat: can handle K symbol odds

--- src/PayLine.ts
+++ src/PayLine.ts
@@ -17,7 +17,9 @@ export class PayLine {
       new Odd('A', 5, 20),
       new Odd('A', 4, 15),
       new Odd('A', 3, 10),
-      new Odd('K', 5, 15)
+      new Odd('K', 5, 15),
+      new Odd('K', 4, 10),
+      new Odd('K', 3, 8)
     ];
 
     return (
