1f4eb3107a255f01654048c74d5e4647024694a4
titangene <titangene.tw@gmail.com>
2026-01-23 23:41:26 +0800

    feat: add nextGameType field in SpinResult class

--- src/SpinResult.ts
+++ src/SpinResult.ts
@@ -1,10 +1,11 @@
 export class SpinResult {
   private constructor(
     public odd: number,
-    public screen: string[][]
+    public screen: string[][],
+    public nextGameType: string
   ) {}
 
   static of(odd: number, screen: string[][]): SpinResult {
-    return new SpinResult(odd, screen);
+    return new SpinResult(odd, screen, 'BASE_GAME');
   }
 }
