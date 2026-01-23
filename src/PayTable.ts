7dd76448331da117f47b0b398f9106181dfedc45
titangene <titangene.tw@gmail.com>
2026-01-23 10:48:44 +0800

    refactor: update isHit() to accept Bet as a parameter

--- src/PayTable.ts
+++ src/PayTable.ts
@@ -3,23 +3,22 @@ import { Bet } from './Bet.ts';
 
 export class PayTable {
   getOdd(betLines: string[], reels: Reels): number {
-    if (reels.isRowHit(0) && this.isHit(betLines, 'L1')) {
+    if (reels.isRowHit(0) && this.isHit(betLines, 'L1', new Bet(betLines))) {
       return 20;
     }
 
-    if (reels.isRowHit(1) && this.isHit(betLines, 'L2')) {
+    if (reels.isRowHit(1) && this.isHit(betLines, 'L2', new Bet(betLines))) {
       return 20;
     }
 
-    if (reels.isRowHit(2) && this.isHit(betLines, 'L3')) {
+    if (reels.isRowHit(2) && this.isHit(betLines, 'L3', new Bet(betLines))) {
       return 20;
     }
 
     return 0;
   }
 
-  private isHit(betLines: string[], line: string): boolean {
-    const bet = new Bet(betLines);
+  private isHit(betLines: string[], line: string, bet: Bet): boolean {
     return bet.includes(line);
   }
 }
