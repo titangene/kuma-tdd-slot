72009c334b2968d6c9c5c9897d02d1b36ba703e3
titangene <titangene.tw@gmail.com>
2026-01-23 10:19:01 +0800

    refactor: extract ProbabilitySystem#isHit()

--- src/ProbabilitySystem.ts
+++ src/ProbabilitySystem.ts
@@ -6,28 +6,23 @@ export class ProbabilitySystem {
   spin(...betLines: string[]): number {
     this.reels.spin();
 
-    if (
-      this.reels.isRowHit(0) &&
-      betLines.filter(betLine => betLine === 'L1').length > 0
-    ) {
+    if (this.reels.isRowHit(0) && this.isHit(betLines, 'L1')) {
       return 20;
     }
 
-    if (
-      this.reels.isRowHit(1) &&
-      betLines.filter(betLine => betLine === 'L2').length > 0
-    ) {
+    if (this.reels.isRowHit(1) && this.isHit(betLines, 'L2')) {
       return 20;
     }
 
-    if (
-      this.reels.isRowHit(2) &&
-      betLines.filter(betLine => betLine === 'L3').length > 0
-    ) {
+    if (this.reels.isRowHit(2) && this.isHit(betLines, 'L3')) {
       return 20;
     }
 
     return 0;
+  }
+
+  private isHit(betLines: string[], line: string) {
+    return betLines.filter(betLine => betLine === line).length > 0;
   }
 
   static create(reels: Reels): ProbabilitySystem {
