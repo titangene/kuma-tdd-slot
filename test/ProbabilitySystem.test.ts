96cb5d4e1e1d0cb39bedb5648f75e4c97efeba20
titangene <titangene.tw@gmail.com>
2026-01-24 00:47:53 +0800

    red: Get Screen in Free Game (compile fail)

--- test/ProbabilitySystem.test.ts
+++ test/ProbabilitySystem.test.ts
@@ -570,4 +570,24 @@ describe('probability system', () => {
       )
     );
   });
+
+  test('Get Screen in Free Game', () => {
+    const sut = ProbabilitySystem.create(
+      Reels.create(new DesignatedNumberGenerator(0, 0, 0, 0, 0), [
+        ['A', 'K', 'A', '10', 'J', 'Q'],
+        ['A', 'K', 'S', 'J', 'Q', 'K'],
+        ['A', 'S', 'A', 'Q', 'K', '10'],
+        ['A', 'S', 'K', '10', 'J', 'Q'],
+        ['A', '10', 'J', 'J', 'Q', 'K']
+      ]),
+      new PayTable(
+        [PayLine.from('L1', [0, 0, 0, 0, 0])],
+        new Odds([new Odd('A', 5, 20)])
+      )
+    );
+
+    sut.spin(new Bet('L1'));
+
+    expect(sut.getScreen()).toBeDefined();
+  });
 });
