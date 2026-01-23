de1546d6d5977ef8a27adb5dd242c8ec9bbd1509
titangene <titangene.tw@gmail.com>
2026-01-23 15:22:49 +0800

    refactor: move getOdd() to Odds class (kill Feature Envy)

--- src/Odds.ts
+++ src/Odds.ts
@@ -1,5 +1,10 @@
 import { Odd } from './Odd.ts';
+import type { Hit } from './Hit.ts';
 
 export class Odds {
   constructor(public rawOdds: Odd[]) {}
+
+  getOdd(hit: Hit) {
+    return this.rawOdds.find(odd => odd.matches(hit))?.odd ?? 0;
+  }
 }
