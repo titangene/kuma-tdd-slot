4b8257c6c9ff44a82cabda1742698b13bf526cff
titangene <titangene.tw@gmail.com>
2026-01-23 10:10:58 +0800

    red: Roll then Row2 hit, bet L1L2L3 -> 20

--- test/ProbabilitySystem.test.ts
+++ test/ProbabilitySystem.test.ts
@@ -94,4 +94,17 @@ describe('probability system', () => {
     );
     expect(sut.spin('L3')).toBe(20);
   });
+
+  test('Roll then Row2 hit, bet L1L2L3 -> 20', () => {
+    const sut = ProbabilitySystem.create(
+      Reels.create(new DesignatedNumberGenerator(1, 1, 1, 1, 1), [
+        ['A', 'Q', 'K'],
+        ['A', 'Q', 'K'],
+        ['A', 'Q', 'K'],
+        ['A', 'Q', 'K'],
+        ['10', 'J', 'K']
+      ])
+    );
+    expect(sut.spin('L1', 'L2', 'L3')).toBe(20);
+  });
 });
