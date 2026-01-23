f8ed7fd4a2a8466748a237a3d03f74971299296d
titangene <titangene.tw@gmail.com>
2026-01-23 10:49:55 +0800

    refactor: reuse Bet instance in getOdd() to improve efficiency

--- src/PayTable.ts
+++ src/PayTable.ts
@@ -3,15 +3,17 @@ import { Bet } from './Bet.ts';
 
 export class PayTable {
   getOdd(betLines: string[], reels: Reels): number {
-    if (reels.isRowHit(0) && this.isHit(new Bet(betLines), 'L1')) {
+    const bet = new Bet(betLines);
+
+    if (reels.isRowHit(0) && this.isHit(bet, 'L1')) {
       return 20;
     }
 
-    if (reels.isRowHit(1) && this.isHit(new Bet(betLines), 'L2')) {
+    if (reels.isRowHit(1) && this.isHit(bet, 'L2')) {
       return 20;
     }
 
-    if (reels.isRowHit(2) && this.isHit(new Bet(betLines), 'L3')) {
+    if (reels.isRowHit(2) && this.isHit(bet, 'L3')) {
       return 20;
     }
 
