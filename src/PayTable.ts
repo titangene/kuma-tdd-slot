016ba5b9abc8979d88304d6258f6a2ecabbc3927
titangene <titangene.tw@gmail.com>
2026-01-23 13:27:02 +0800

    refactor: move payLines initialization to constructor

--- src/PayTable.ts
+++ src/PayTable.ts
@@ -3,12 +3,16 @@ import { Screen } from './Screen.ts';
 import { PayLine } from './PayLine.ts';
 
 export class PayTable {
-  payLines: PayLine[] = [
-    new PayLine('L1', [0, 0, 0, 0, 0]),
-    new PayLine('L2', [1, 1, 1, 1, 1]),
-    new PayLine('L3', [2, 2, 2, 2, 2]),
-    new PayLine('L4', [0, 1, 2, 1, 0])
-  ];
+  payLines: PayLine[];
+
+  constructor() {
+    this.payLines = [
+      new PayLine('L1', [0, 0, 0, 0, 0]),
+      new PayLine('L2', [1, 1, 1, 1, 1]),
+      new PayLine('L3', [2, 2, 2, 2, 2]),
+      new PayLine('L4', [0, 1, 2, 1, 0])
+    ];
+  }
 
   getOdd(screen: Screen, bet: Bet): number {
     return this.payLines.reduce(
