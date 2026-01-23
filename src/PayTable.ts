5b03ff71a10bf5b28fb80029e72543cb2604aa49
titangene <titangene.tw@gmail.com>
2026-01-23 15:15:56 +0800

    refactor: extract field

--- src/PayTable.ts
+++ src/PayTable.ts
@@ -7,22 +7,18 @@ import { Odd } from './Odd.ts';
 export class PayTable {
   constructor(public payLines: PayLine[]) {}
 
+  private odds = new Odds([
+    new Odd('A', 5, 20),
+    new Odd('A', 4, 15),
+    new Odd('A', 3, 10),
+    new Odd('K', 5, 15),
+    new Odd('K', 4, 10),
+    new Odd('K', 3, 8)
+  ]);
+
   getOdd(screen: Screen, bet: Bet): number {
     return this.payLines.reduce(
-      (odd, payLine) =>
-        odd +
-        payLine.getOdd(
-          screen,
-          bet,
-          new Odds([
-            new Odd('A', 5, 20),
-            new Odd('A', 4, 15),
-            new Odd('A', 3, 10),
-            new Odd('K', 5, 15),
-            new Odd('K', 4, 10),
-            new Odd('K', 3, 8)
-          ])
-        ),
+      (totalOdd, payLine) => totalOdd + payLine.getOdd(screen, bet, this.odds),
       0
     );
   }
