60a9e885fe10c8acc1d335dc2a3d8fb657989466
titangene <titangene.tw@gmail.com>
2026-01-23 10:33:37 +0800

    refactor: delegate getOdd() to PayTable class

--- src/PayTable.ts
+++ src/PayTable.ts
@@ -1,1 +1,23 @@
-export class PayTable {}
+import type { Reels } from './Reels.ts';
+
+export class PayTable {
+  getOdd(betLines: string[], reels: Reels): number {
+    if (reels.isRowHit(0) && this.isHit(betLines, 'L1')) {
+      return 20;
+    }
+
+    if (reels.isRowHit(1) && this.isHit(betLines, 'L2')) {
+      return 20;
+    }
+
+    if (reels.isRowHit(2) && this.isHit(betLines, 'L3')) {
+      return 20;
+    }
+
+    return 0;
+  }
+
+  private isHit(betLines: string[], line: string): boolean {
+    return betLines.filter(betLine => betLine === line).length > 0;
+  }
+}
--- src/ProbabilitySystem.ts
+++ src/ProbabilitySystem.ts
@@ -9,27 +9,7 @@ export class ProbabilitySystem {
 
   spin(...betLines: string[]): number {
     this.reels.spin();
-    return this.getOdd(betLines, this.reels);
-  }
-
-  private getOdd(betLines: string[], reels: Reels): number {
-    if (reels.isRowHit(0) && this.isHit(betLines, 'L1')) {
-      return 20;
-    }
-
-    if (reels.isRowHit(1) && this.isHit(betLines, 'L2')) {
-      return 20;
-    }
-
-    if (reels.isRowHit(2) && this.isHit(betLines, 'L3')) {
-      return 20;
-    }
-
-    return 0;
-  }
-
-  private isHit(betLines: string[], line: string) {
-    return betLines.filter(betLine => betLine === line).length > 0;
+    return this.payTable.getOdd(betLines, this.reels);
   }
 
   static create(reels: Reels): ProbabilitySystem {
