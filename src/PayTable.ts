2d55bd01d8a2dbcc97a9fb9018c58cef69de78ca
titangene <titangene.tw@gmail.com>
2026-01-23 12:00:08 +0800

    green: Roll then Row1 Row3 hit, bet L1L2L3 -> 40 (multiple hits)

--- src/PayTable.ts
+++ src/PayTable.ts
@@ -3,18 +3,18 @@ import { Screen } from './Screen.ts';
 
 export class PayTable {
   getOdd(screen: Screen, bet: Bet): number {
+    let odd = 0;
+
     if (screen.isScreenRowHit(0) && bet.includes('L1')) {
-      return 20;
+      odd += 20;
     }
-
     if (screen.isScreenRowHit(1) && bet.includes('L2')) {
-      return 20;
+      odd += 20;
     }
-
     if (screen.isScreenRowHit(2) && bet.includes('L3')) {
-      return 20;
+      odd += 20;
     }
 
-    return 0;
+    return odd;
   }
 }
