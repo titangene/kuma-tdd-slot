f31484898a19ab8a826b2a56e0ead7b7c0952f3a
titangene <titangene.tw@gmail.com>
2026-01-23 10:13:08 +0800

    green: Roll then Row2 hit, bet L1L2L3 -> 20

--- src/ProbabilitySystem.ts
+++ src/ProbabilitySystem.ts
@@ -3,18 +3,27 @@ import { Reels } from './Reels.ts';
 export class ProbabilitySystem {
   private constructor(private reels: Reels) {}
 
-  spin(betLine: string): number {
+  spin(...betLines: string[]): number {
     this.reels.spin();
 
-    if (this.reels.isRowHit(0) && betLine === 'L1') {
+    if (
+      this.reels.isRowHit(0) &&
+      betLines.filter(betLine => betLine === 'L1').length > 0
+    ) {
       return 20;
     }
 
-    if (this.reels.isRowHit(1) && betLine === 'L2') {
+    if (
+      this.reels.isRowHit(1) &&
+      betLines.filter(betLine => betLine === 'L2').length > 0
+    ) {
       return 20;
     }
 
-    if (this.reels.isRowHit(2) && betLine === 'L3') {
+    if (
+      this.reels.isRowHit(2) &&
+      betLines.filter(betLine => betLine === 'L3').length > 0
+    ) {
       return 20;
     }
 
