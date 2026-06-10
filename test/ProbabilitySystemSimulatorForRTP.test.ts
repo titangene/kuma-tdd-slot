e78c9625ec04e4768d7ac95b23a04a198866f220
titangene <titangene.tw@gmail.com>
2026-06-10 17:20:46 +0800

    feat: simulator ok, but RTP too large

--- test/ProbabilitySystemSimulatorForRTP.test.ts
+++ test/ProbabilitySystemSimulatorForRTP.test.ts
@@ -8,6 +8,7 @@ import { Odd } from '@/Odd.ts';
 import { Screen } from '@/Screen.ts';
 import { ProbabilitySystem } from '@/ProbabilitySystem.ts';
 import { NativeRandomNumberGenerator } from '@/NativeRandomNumberGenerator.ts';
+import { Bet } from '@/Bet.ts';
 
 describe('probability system simulator', () => {
   test('RTP Simulator', () => {
@@ -99,6 +100,24 @@ describe('probability system simulator', () => {
       ),
       (screen: Screen): number => (screen.countSymbol('S') >= 5 ? 10 : 0)
     );
+
     const sut = ProbabilitySystem.create(baseGame, freeGame);
+
+    let nextGameType = sut.getNextGameType();
+    const rounds = 1_000_000;
+    let totalOdd = 0;
+    for (let i = 0; i < rounds; i++) {
+      const spinResult =
+        'BASE_GAME' === nextGameType
+          ? sut.spin(
+              new Bet('L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8', 'L9')
+            )
+          : sut.spinFree();
+
+      totalOdd += spinResult.odd;
+      nextGameType = spinResult.nextGameType;
+    }
+
+    console.log(`Total odd: ${totalOdd}, Average odd: ${totalOdd / rounds}`);
   });
 });
