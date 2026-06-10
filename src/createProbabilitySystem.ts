b7a76adcadb357e0b1ccb218539150d312bc8dce
titangene <titangene.tw@gmail.com>
2026-06-10 18:50:45 +0800

    refactor: move createProbabilitySystem to its own module

---
+++ src/createProbabilitySystem.ts
@@ -0,0 +1,57 @@
+import type { ProbabilitySystemSettings } from '@/ProbabilitySystemSettings.ts';
+import { ProbabilitySystem } from '@/ProbabilitySystem.ts';
+import { SlotGame } from '@/SlotGame.ts';
+import { Reels } from '@/Reels.ts';
+import { NativeRandomNumberGenerator } from '@/NativeRandomNumberGenerator.ts';
+import { PayTable } from '@/PayTable.ts';
+import { PayLine } from '@/PayLine.ts';
+import { Odds } from '@/Odds.ts';
+import { Odd } from '@/Odd.ts';
+import { Screen } from '@/Screen.ts';
+
+export function createProbabilitySystem(
+  settings: ProbabilitySystemSettings
+): ProbabilitySystem {
+  const baseGameSettings = settings.baseGameSettings;
+  const baseGame: SlotGame = SlotGame.of(
+    Reels.create(new NativeRandomNumberGenerator(), baseGameSettings.reels),
+    new PayTable(
+      baseGameSettings.payLines.map(payLine =>
+        PayLine.from(payLine.name, payLine.indexes)
+      ),
+      new Odds(
+        baseGameSettings.odds.map(
+          odd => new Odd(odd.symbol, odd.count, odd.odd)
+        )
+      )
+    ),
+    (screen: Screen): number =>
+      screen.countSymbol(baseGameSettings.freeGameIncrementParameters.symbol) >=
+      baseGameSettings.freeGameIncrementParameters.count
+        ? baseGameSettings.freeGameIncrementParameters.increment
+        : 0
+  );
+
+  const freeGameSettings = settings.freeGameSettings;
+  const freeGame: SlotGame = SlotGame.of(
+    Reels.create(new NativeRandomNumberGenerator(), freeGameSettings.reels),
+    new PayTable(
+      freeGameSettings.payLines.map(payLine =>
+        PayLine.from(payLine.name, payLine.indexes)
+      ),
+      new Odds(
+        freeGameSettings.odds.map(
+          odd => new Odd(odd.symbol, odd.count, odd.odd)
+        )
+      )
+    ),
+    (screen: Screen): number =>
+      screen.countSymbol(freeGameSettings.freeGameIncrementParameters.symbol) >=
+      freeGameSettings.freeGameIncrementParameters.count
+        ? freeGameSettings.freeGameIncrementParameters.increment
+        : 0
+  );
+
+  const sut = ProbabilitySystem.create(baseGame, freeGame);
+  return sut;
+}
--- test/ProbabilitySystemSimulatorForRTP.test.ts
+++ test/ProbabilitySystemSimulatorForRTP.test.ts
@@ -1,62 +1,6 @@
 import { describe, test } from 'vitest';
-import { SlotGame } from '@/SlotGame.ts';
-import { Reels } from '@/Reels.ts';
-import { PayTable } from '@/PayTable.ts';
-import { PayLine } from '@/PayLine.ts';
-import { Odds } from '@/Odds.ts';
-import { Odd } from '@/Odd.ts';
-import { Screen } from '@/Screen.ts';
-import { ProbabilitySystem } from '@/ProbabilitySystem.ts';
-import { NativeRandomNumberGenerator } from '@/NativeRandomNumberGenerator.ts';
 import { Bet } from '@/Bet.ts';
-import type { ProbabilitySystemSettings } from '@/ProbabilitySystemSettings.ts';
-
-function createProbabilitySystem(
-  settings: ProbabilitySystemSettings
-): ProbabilitySystem {
-  const baseGameSettings = settings.baseGameSettings;
-  const baseGame: SlotGame = SlotGame.of(
-    Reels.create(new NativeRandomNumberGenerator(), baseGameSettings.reels),
-    new PayTable(
-      baseGameSettings.payLines.map(payLine =>
-        PayLine.from(payLine.name, payLine.indexes)
-      ),
-      new Odds(
-        baseGameSettings.odds.map(
-          odd => new Odd(odd.symbol, odd.count, odd.odd)
-        )
-      )
-    ),
-    (screen: Screen): number =>
-      screen.countSymbol(baseGameSettings.freeGameIncrementParameters.symbol) >=
-      baseGameSettings.freeGameIncrementParameters.count
-        ? baseGameSettings.freeGameIncrementParameters.increment
-        : 0
-  );
-
-  const freeGameSettings = settings.freeGameSettings;
-  const freeGame: SlotGame = SlotGame.of(
-    Reels.create(new NativeRandomNumberGenerator(), freeGameSettings.reels),
-    new PayTable(
-      freeGameSettings.payLines.map(payLine =>
-        PayLine.from(payLine.name, payLine.indexes)
-      ),
-      new Odds(
-        freeGameSettings.odds.map(
-          odd => new Odd(odd.symbol, odd.count, odd.odd)
-        )
-      )
-    ),
-    (screen: Screen): number =>
-      screen.countSymbol(freeGameSettings.freeGameIncrementParameters.symbol) >=
-      freeGameSettings.freeGameIncrementParameters.count
-        ? freeGameSettings.freeGameIncrementParameters.increment
-        : 0
-  );
-
-  const sut = ProbabilitySystem.create(baseGame, freeGame);
-  return sut;
-}
+import { createProbabilitySystem } from '@/createProbabilitySystem.ts';
 
 describe('probability system simulator', () => {
   test('RTP Simulator', () => {
